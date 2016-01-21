a33b4d7c5d360f090d3ab09b480751497fc05629
Sebastian Heuchler <git@01h.de>
2016-01-21 14:17:53 +0100

    add main log class

---
+++ src/Unlog/LogBlock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unlog
+{
+	public class LogBlock : IDisposable
+	{
+		private bool _WasDisposed = false;
+
+		public bool Keep = true;
+
+		public void Dispose ()
+		{
+			if (!_WasDisposed) {
+				_WasDisposed = true;
+				Log.DoLeave (Keep);
+			}
+		}
+	}
+}
