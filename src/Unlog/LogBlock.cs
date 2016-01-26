023544b53c5690b6a90347049cb4431ca671f7d9
Sebastian Heuchler <git@01h.de>
2016-01-26 13:51:26 +0100

    codedoc

--- src/Unlog/LogBlock.cs
+++ src/Unlog/LogBlock.cs
@@ -5,8 +5,14 @@ using System.Text;
 
 namespace Unlog
 {
+	/// <summary>
+	/// A local log block. Is stashed and kept by default.
+	/// </summary>
 	public class LogBlock : IDisposable
 	{
+		/// <summary>
+		/// Set to false to discard this block on dispose instead of writing it to the log output.
+		/// </summary>
 		private bool _WasDisposed = false;
 
 		public bool Keep = true;
